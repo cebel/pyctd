de8bec73dd6f69ed0048925d96b821b86fd764c0
Christian Ebeling <Chr.Ebeling@gmail.com>
2017-05-19 07:36:41 +0200

    table_config and model adapted to changes in table exporsureevents

--- src/pyctd/manager/defaults.py
+++ src/pyctd/manager/defaults.py
@@ -4,14 +4,14 @@
 This file contains a listing of the default CTD URLs.
 """
 from .table_conf import tables
-import os
-from ..constants import DEFAULT_CACHE_LOCATION
+from ..constants import DEFAULT_DATABASE_LOCATION, DEFAULT_SQLITE_TEST_DATABASE_NAME
 
 url_base = "http://ctdbase.org/reports/"
 
+sqlalchemy_connection_string_default = 'sqlite:///' + DEFAULT_DATABASE_LOCATION
+sqlalchemy_connection_string_4_tests = 'sqlite:///' + DEFAULT_SQLITE_TEST_DATABASE_NAME
+
 sqlalchemy_connection_string_4_mysql = 'mysql+pymysql://pyctd:pyctd@localhost/pyctd?charset=utf8'
-sqlalchemy_connection_string_default = 'sqlite:///' + DEFAULT_CACHE_LOCATION
-sqlalchemy_connection_string_4_tests = 'sqlite:////home/ceb/temp/pyctd.db'
 sqlalchemy_connection_string_4_mysql_tests = 'mysql+pymysql://pyctd:pyctd@localhost/pyctd_test?charset=utf8'
 
 urls = [url_base + tables[table]['file_name'] for table in tables]
