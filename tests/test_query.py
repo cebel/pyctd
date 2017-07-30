45450899b33be88d8f5d7aca112fa648289d0e0d
Charles Tapley Hoyt <cthoyt@gmail.com>
2017-07-30 11:04:56 +0200

    Updated dummy method
    Since we’re passing these as kwargs sometimes, the dummy method should
    take in variadic arguments as well

--- tests/test_query.py
+++ tests/test_query.py
@@ -1,23 +1,51 @@
 # -*- coding: utf-8 -*-
 
 import logging
-import unittest
 import os
-from pyctd.manager.models import GeneAltGeneId, Pathway, DiseasePathway, Disease, Chemical, ChemGeneIxn, Gene,\
-    ChemicalDisease, GeneDiseasePubmed, GeneDiseaseOmim, GeneDisease, ChemGeneIxnGeneForm, ChemGeneIxnPubmed, \
-    ChemGeneIxnInteractionAction, ChemicalDiseasePubmedid, ChemicalDiseaseOmim, ChemicalSynonym, ChemicalDrugbank, \
-    ChemicalTreenumber, ChemicalParentid, GeneUniprot, GeneSynonym, GenePharmgkb, GenePathway, GeneBiogrid, \
-    DiseaseSynonym, DiseaseSlimmapping, DiseaseAltdiseaseid, Action, ChemGoEnriched, ChemicalParenttreenumber, \
-    ChemPathwayEnriched, ExposureEvent
-from pyctd.manager.defaults import DEFAULT_SQLITE_TEST_DATABASE_NAME
+import shutil
+import unittest
 
 import pyctd
-import shutil
+from pyctd.constants import PYCTD_DATA_DIR
 from pyctd.manager import table_conf
 from pyctd.manager.database import DbManager, BaseDbManager
+from pyctd.manager.defaults import DEFAULT_SQLITE_TEST_DATABASE_NAME, sqlalchemy_connection_string_4_tests
+from pyctd.manager.models import (
+    GeneAltGeneId,
+    Pathway,
+    DiseasePathway,
+    Disease,
+    Chemical,
+    ChemGeneIxn,
+    Gene,
+    ChemicalDisease,
+    GeneDiseasePubmed,
+    GeneDiseaseOmim,
+    GeneDisease,
+    ChemGeneIxnGeneForm,
+    ChemGeneIxnPubmed,
+    ChemGeneIxnInteractionAction,
+    ChemicalDiseasePubmedid,
+    ChemicalDiseaseOmim,
+    ChemicalSynonym,
+    ChemicalDrugbank,
+    ChemicalTreenumber,
+    ChemicalParentid,
+    GeneUniprot,
+    GeneSynonym,
+    GenePharmgkb,
+    GenePathway,
+    GeneBiogrid,
+    DiseaseSynonym,
+    DiseaseSlimmapping,
+    DiseaseAltdiseaseid,
+    Action,
+    ChemGoEnriched,
+    ChemicalParenttreenumber,
+    ChemPathwayEnriched,
+    ExposureEvent
+)
 from pyctd.manager.query import QueryManager
-from pyctd.constants import PYCTD_DATA_DIR
-from pyctd.manager.defaults import sqlalchemy_connection_string_4_tests
 
 log = logging.getLogger(__name__)
 
@@ -30,13 +58,10 @@ test_data_folder = os.path.join(PYCTD_DATA_DIR, 'tests')
 if not os.path.exists(test_data_folder):
     os.mkdir(test_data_folder)
 
+
 @staticmethod
-def download_urls(dummy1, dummy2):
-    """
-    overwrites pyctd.manager.database.DbManager.download_urls in TestImport.setup
-    :param dummy1: dummy parameter
-    :param dummy2: dummy parameter
-    """
+def download_urls(*args, **kwargs):
+    """overwrites pyctd.manager.database.DbManager.download_urls in TestImport.setup"""
 
     file_names = [x['file_name'] for x in list(table_conf.tables.values())]
     for file_name in file_names:
@@ -120,7 +145,7 @@ class TestImport(unittest.TestCase):
         self.assertEqual(r.gene_name, 'GeneName1')
         self.assertEqual(r.gene_symbol, 'GeneSymbol1')
         self.assertEqual([x.pharmgkb_id for x in r.pharmgkb_ids], ['PharmGKBID1_1', 'PharmGKBID1_2'])
-        self.assertEqual([x.synonym for x in r.synonyms] ,['Synonym1_1', 'Synonym1_2'])
+        self.assertEqual([x.synonym for x in r.synonyms], ['Synonym1_1', 'Synonym1_2'])
         self.assertEqual([x.uniprot_id for x in r.uniprot_ids], ['UniProtID1_1', 'UniProtID1_2'])
         self.session.commit()
 
@@ -139,25 +164,29 @@ class TestImport(unittest.TestCase):
 
     def test_get_chem_gene_interaction_actions(self):
         r = self.query.get_chem_gene_interaction_actions()[0]
-        #self.assertEqual()
+        # self.assertEqual()
 
     def test_gene_forms(self):
         r = self.query.gene_forms
-        self.assertEqual(r, ['GeneForm1_1',
-                                'GeneForm1_2',
-                                'GeneForm2_1',
-                                'GeneForm2_2',
-                                'GeneForm3_1',
-                                'GeneForm3_2'])
+        self.assertEqual(r, [
+            'GeneForm1_1',
+            'GeneForm1_2',
+            'GeneForm2_1',
+            'GeneForm2_2',
+            'GeneForm3_1',
+            'GeneForm3_2'
+        ])
 
     def test_interaction_actions(self):
         result = self.query.interaction_actions
-        expected_result = ['InteractionActions1',
-                            'InteractionActions2',
-                            'InteractionActions3',
-                            'InteractionActions4',
-                            'InteractionActions5',
-                            'InteractionActions6']
+        expected_result = [
+            'InteractionActions1',
+            'InteractionActions2',
+            'InteractionActions3',
+            'InteractionActions4',
+            'InteractionActions5',
+            'InteractionActions6'
+        ]
         self.assertEqual(result, expected_result)
 
     def test_actions(self):
@@ -171,7 +200,7 @@ class TestImport(unittest.TestCase):
         r = self.query.get_gene_disease()[0]
         self.assertEqual(r.direct_evidence, 'DirectEvidence1')
         self.assertEqual(r.disease.disease_name, 'DiseaseName1')
-        #self.assertEqual(r.gene)
+        # self.assertEqual(r.gene)
 
     def test_direct_evidences(self):
         direct_evidences = self.query.direct_evidences
@@ -185,16 +214,16 @@ class TestImport(unittest.TestCase):
     def test_get_gene_pathways(self):
         gene_pathways = self.query.get_gene_pathways()[0]
 
-    #def test_get_go_enriched__by__chemical_name(self):
+    # def test_get_go_enriched__by__chemical_name(self):
     #    r = self.query.get_go_enriched__by__chemical_name(chemical_name='ChemicalName1')[0]
 
-    #def test_get_pathway_enriched__by__chemical_name(self):
+    # def test_get_pathway_enriched__by__chemical_name(self):
     #    r = self.query.get_pathway_enriched__by__chemical_name(chemical_name='ChemicalName1')[0]
 
-    #def test_get_therapeutic_chemical__by__disease_name(self):
+    # def test_get_therapeutic_chemical__by__disease_name(self):
     #    r = self.query.get_therapeutic_chemical__by__disease_name(disease_name='DiseaseName1', limit=1)[0]
 
-    #def test_get_marker_chemical__by__disease_name(self):
+    # def test_get_marker_chemical__by__disease_name(self):
     #    r = self.query.get_marker_chemical__by__disease_name(disease_name='DiseaseName1', limit=1)[0]
 
     def test_get_chemical__by__disease(self):
@@ -202,4 +231,3 @@ class TestImport(unittest.TestCase):
 
     def get_action(self):
         action = self.query.get_action()[0]
-
