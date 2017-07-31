7bba342172cd0a2e312b106523160cdc2d2ac8f3
Christian Ebeling <chr.ebeling@gmail.com>
2017-07-31 14:13:15 +0200

    Bump version: 0.5.9-dev → 0.5.9

--- src/pyctd/__init__.py
+++ src/pyctd/__init__.py
@@ -21,7 +21,7 @@ from .manager.query import QueryManager as query
 
 __all__ = ['update', 'query', 'set_connection', 'set_mysql_connection']
 
-__version__ = '0.5.9-dev'
+__version__ = '0.5.9'
 
 __title__ = 'PyCTD'
 __description__ = 'Importing and querying CTD'
